69bcd8a2734da162481560d82309dd40a644db56
Tri <tri@triandco.com>
2022-11-28 17:04:24 +1100

    clean up code

--- SentenceEmbedding.Tests/WordPieceTokenizerTests.fs
+++ SentenceEmbedding.Tests/WordPieceTokenizerTests.fs
@@ -8,7 +8,6 @@ let ``My test`` () =
   let input = "unaffable"
   let expected = Set.ofList ["una"; "##af"; "##ffa"; "##able"]
 
-  printf "running heck mate"
   input 
   |> tokenize (WordPieceTokeniserOptions.defaultValue ())
   |> Set.ofSeq
--- SentenceEmbedding/WordPieceTokenizer.fs
+++ SentenceEmbedding/WordPieceTokenizer.fs
@@ -15,7 +15,7 @@ module WordPieceTokeniserOptions =
   let DEFAULT_MAX_INPUT_CHAR_PER_WORD = 100
 
   let defaultValue () = {
-    Vocab = WordPieceTokeniserOptions.DEFAULT_VOCAB.Split("\n") 
+    Vocab = WordPieceTokeniserOptions.DEFAULT_VOCAB.Split("\r\n") 
     UNKToken = DEFAULT_UNK_TOKEN
     MaxInputCharPerWord = DEFAULT_MAX_INPUT_CHAR_PER_WORD
   }
@@ -25,31 +25,31 @@ module WordPieceTokeniserOptions =
 // More about WordPiece tokenizer visit [huggingface](https://huggingface.co/course/chapter6/6?fw=pt)
 let tokenize (options: WordPieceTokeniserOptions) text = 
 
-    let processToken (token: string) : string seq =
+    let toToken (token: string) : string seq =
       if String.length token > options.MaxInputCharPerWord then [options.UNKToken] else
       
-      let rec a (startIndex: int) (chars: char seq) : Option<string seq> = 
+      let rec getToken (startIndex: int) (chars: char seq) : Option<string seq> = 
         let endIndex = Seq.length chars
         if startIndex >= endIndex then Some [] else
         
-          let rec b (begining: int) (ending: int) (charss: char seq) : Option<string * int> =
+          let rec getSubToken (begining: int) (ending: int) (charss: char seq) : Option<string * int> =
             if begining >= ending then None else
             let substr = charss |> Seq.toArray |> fun e -> e[begining..ending] |> System.String.Concat
             let sub = if begining > 0 then $"##{substr}" else substr
             if options.Vocab |> Seq.contains sub then Some (sub, ending)
-            else b begining (ending - 1) charss
+            else getSubToken begining (ending - 1) charss
 
-          match b startIndex endIndex chars with 
+          match getSubToken startIndex endIndex chars with 
           | Some (substr, ending) ->
-             let t = a ending chars |> Option.map (Seq.append [| substr |])
+             let t = getToken ending chars |> Option.map (Seq.append [| substr |])
              t
           | None -> None
 
-      a 0 token |> Option.defaultValue [ options.UNKToken ]
+      getToken 0 token |> Option.defaultValue [ options.UNKToken ]
 
     let whitespace_tokenize (text: string) = text.Trim().Split(" ")
     
     text 
     |> whitespace_tokenize
-    |> Array.map processToken
+    |> Array.map toToken
     |> Seq.concat
